db0b47c1ea6c7b38acc0f0168a0b97ed911b612b
Copper Phosphate <cu3po42@gmail.com>
2017-08-31 17:32:56 +0200

    Document searchmem

--- src/searchmem.ts
+++ src/searchmem.ts
@@ -1,8 +1,17 @@
 import NtrClient from './ntrclient';
 
+/**
+ * Search for a given uint32 value in the memory of a process.
+ * 
+ * @param client The NTRClient which should be used to access memory
+ * @param pid The PID of the target process
+ * @param value The uint32 value for which to search
+ * @param locations A possible list of locations to check. If this argument is omitted, check all locations
+ * @return A list of locations where the value was found
+ */
 export default async function search(client: NtrClient, pid: number, value: number, locations?: number[]) {
   if (locations !== undefined) {
-    const res = [];
+    const res: number[] = [];
 
     for (const loc of locations) {
       const val = await client.readMemory(loc, 4, pid);
@@ -15,7 +24,7 @@ export default async function search(client: NtrClient, pid: number, value: numb
   }
 
   const memorymap = await client.getMemlayout(pid);
-  const res = [];
+  const res: number[] = [];
   for (const region of memorymap) {
     console.log(`Scanning region from ${region.start.toString(16)} to ${region.end.toString(16)}`);
     const mem = await client.readMemory(region.start, region.size, pid);
